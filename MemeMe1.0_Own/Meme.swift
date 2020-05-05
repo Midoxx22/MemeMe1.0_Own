4dc453bce99ec29bc0cccd91a1cf4853210affc6
Patrick Sauerwein <sauerwein.patrick@gmail.com>
2020-05-05 13:22:26 +0200

    For Udacity Question

---
+++ MemeMe1.0_Own/Meme.swift
@@ -0,0 +1,17 @@
+//
+//  Meme.swift
+//  MemeMe1.0_Own
+//
+//  Created by Patrick Sauerwein on 05.05.20.
+//  Copyright © 2020 Patrick Sauerwein. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var topText: String
+    var botText: String
+    var origImage: UIImage
+    var memedImage: UIImage
+}
